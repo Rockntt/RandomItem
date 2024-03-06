d08783a54c7ee95d45a81434b92959f1457cdf3a
Rockntt <klochkovmv2007@gmail.com>
2024-03-06 20:09:13 +0300

    my functions now store in a different file, writing logic

---
+++ src/main/java/ru/rockntt/randomitem/MyFunctions.java
@@ -0,0 +1,29 @@
+package ru.rockntt.randomitem;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.bukkit.entity.Player;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MyFunctions {
+    public static Runnable giveRandomItemToPlayers;
+    private Random random = new Random();
+
+    public void giveRandomItemToPlayers(Player[] playerList) {
+        for (Player player : playerList) {
+            Material[] materials = Material.values(); // Получаем все возможные материалы
+            int randomMaterialIndex = random.nextInt(materials.length); // Генерируем случайный индекс
+            Material randomMaterial = materials[randomMaterialIndex]; // Получаем случайный материал
+            ItemStack item = new ItemStack(randomMaterial, 1); // Создаем предмет с случайным материалом
+            player.getInventory().addItem(item); // Выдаем предмет игроку
+        }
+    }
+}
--- src/main/java/ru/rockntt/randomitem/RandomItem.java
+++ src/main/java/ru/rockntt/randomitem/RandomItem.java
@@ -1,8 +1,11 @@
 package ru.rockntt.randomitem;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.bukkit.entity.Player;
@@ -10,12 +13,17 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.List;
 
+import ru.rockntt.randomitem.MyFunctions;
+
 public final class RandomItem extends JavaPlugin {
 
     private List<Player> playerList = new ArrayList<>();
+    private BukkitTask itemGiveTask;
+    private boolean isSchedulerRunning = false;
 
     @Override
     public void onEnable() {
+        Bukkit.getScheduler().runTaskTimer(this, MyFunctions.giveRandomItemToPlayers, 0, 600); // 20 тиков = 1 секунда
         getCommand("randomitem").setExecutor(new CommandExecutor() {
             @Override
             public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
@@ -65,11 +73,37 @@ public final class RandomItem extends JavaPlugin {
                 playerList.add(player);
             }
         });
+        getCommand("start").setExecutor(new CommandExecutor() {
+            @Override
+            public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+                if (!isSchedulerRunning) {
+                    itemGiveTask = Bukkit.getScheduler().runTaskTimer(RandomItem.this, MyFunctions.giveRandomItemToPlayers, 0, 600);
+                    isSchedulerRunning = true;
+                    sender.sendMessage("Scheduler started");
+                } else {
+                    sender.sendMessage("Scheduler is already running");
+                }
+                return true;
+            }
+        });
 
+        getCommand("stop").setExecutor(new CommandExecutor() {
+            @Override
+            public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+                if (isSchedulerRunning) {
+                    itemGiveTask.cancel();
+                    isSchedulerRunning = false;
+                    sender.sendMessage("Scheduler stopped");
+                } else {
+                    sender.sendMessage("Scheduler is not running");
+                }
+                return true;
+            }
+        });
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        stopSchedulerTask();
     }
 }
