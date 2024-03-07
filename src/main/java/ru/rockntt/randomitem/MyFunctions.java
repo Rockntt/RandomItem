6f9c6ec7f05ff9645e2ea20588c173598b6ce919
Rockntt <klochkovmv2007@gmail.com>
2024-03-07 18:04:53 +0300

    game now works

--- src/main/java/ru/rockntt/randomitem/MyFunctions.java
+++ src/main/java/ru/rockntt/randomitem/MyFunctions.java
@@ -4,6 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 import org.bukkit.entity.Player;
 import org.bukkit.Material;
@@ -14,10 +15,10 @@ import java.util.List;
 import java.util.Random;
 
 public class MyFunctions {
-    public static Runnable giveRandomItemToPlayers;
-    private Random random = new Random();
+//    public static BukkitRunnable giveRandomItemToPlayers;
+    private static Random random = new Random();
 
-    public void giveRandomItemToPlayers(Player[] playerList) {
+    public static void giveRandomItemToPlayers(Player[] playerList) {
         for (Player player : playerList) {
             Material[] materials = Material.values(); // Получаем все возможные материалы
             int randomMaterialIndex = random.nextInt(materials.length); // Генерируем случайный индекс
--- src/main/java/ru/rockntt/randomitem/RandomItem.java
+++ src/main/java/ru/rockntt/randomitem/RandomItem.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,20 +12,26 @@ import org.jetbrains.annotations.NotNull;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import ru.rockntt.randomitem.MyFunctions;
 
 public final class RandomItem extends JavaPlugin {
 
-    private List<Player> playerList = new ArrayList<>();
+    private Player[] playerList = new ArrayList<>().toArray(new Player[0]);
     private BukkitTask itemGiveTask;
+    
     private boolean isSchedulerRunning = false;
     private int item_give_period = 600;
 
     @Override
     public void onEnable() {
-        Bukkit.getScheduler().runTaskTimer(this, MyFunctions.giveRandomItemToPlayers, 0, item_give_period); // 20 тиков = 1 секунда
+        Plugin plugin = this;
+        itemGiveTask = Bukkit.getScheduler().runTaskTimer(this, () -> {
+            MyFunctions.giveRandomItemToPlayers(playerList);
+        }, 0L, item_give_period);
+
         getCommand("randomitem").setExecutor(new CommandExecutor() {
             @Override
             public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
@@ -54,7 +61,7 @@ public final class RandomItem extends JavaPlugin {
                 }
                 if(args[0].equalsIgnoreCase("players")){
 
-                    if (playerList.size() != 0) {
+                    if (playerList.length != 0) {
                         s = new String();
                         s += "Участвующие игроки: ";
                         for (Player k : playerList) {
@@ -71,31 +78,43 @@ public final class RandomItem extends JavaPlugin {
                     if (isSchedulerRunning) {
                         itemGiveTask.cancel();
                         isSchedulerRunning = false;
-                        sender.sendMessage("Scheduler stopped");
+                        sender.sendMessage("Игра остановлена");
                     } else {
-                        sender.sendMessage("Scheduler is not running");
+                        sender.sendMessage("Игра и так не начата");
                     }
                     return true;
                 }
 
                 if(args.length == 1 && args[0].equalsIgnoreCase("start")){
                     if (!isSchedulerRunning) {
-                        itemGiveTask = Bukkit.getScheduler().runTaskTimer(RandomItem.this, MyFunctions.giveRandomItemToPlayers, 0, 600);
+                        Runnable task = new Runnable() {
+                            @Override
+                            public void run() {
+                                // Ваш код для выдачи предметов игрокам
+                                MyFunctions.giveRandomItemToPlayers(playerList);
+                            }
+                        };
+
+
+                        itemGiveTask = Bukkit.getScheduler().runTaskTimer(plugin, task, 0, item_give_period);
+
                         isSchedulerRunning = true;
                         sender.sendMessage("Игра начата!");
                     } else {
-                        sender.sendMessage("Игра остановлена");
+                        sender.sendMessage("Игра уже идет!");
                     }
                     return true;
                 }
                 if(args.length > 2 && (args[0] + " " + args[1]).equalsIgnoreCase("set time")){
                     item_give_period = (int)Math.ceil(Double.parseDouble(args[2])) * 20;
-                    sender.sendMessage(String.format("Период выдачи предметов игрокам установлен на %s", item_give_period));
+                    sender.sendMessage(String.format("Период выдачи предметов игрокам установлен на %s", item_give_period / 20));
                 }
                 return true;
             }
             public void addPlayer(Player player) {
-                playerList.add(player);
+                Player[] newArray = Arrays.copyOf(playerList, playerList.length + 1);
+                newArray[newArray.length - 1] = player;
+                playerList = newArray;
             }
         });
     }
